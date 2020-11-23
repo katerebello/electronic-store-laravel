8aae454c0ecd4897b413e28914ed060902e08f63
Kate Rebello <katerebello27@gmail.com>
2020-11-23 17:17:57 +0530

    user/admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
